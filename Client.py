7e4473edd90379f393ade45f4b725e709027ccfa
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2023-07-01 12:16:00 +0330

    Add login

--- Client.py
+++ Client.py
@@ -1,5 +1,6 @@
 import json
 import socket
+import time
 
 HOST, PORT = 'localhost', 8080
 server = (HOST, PORT)
@@ -9,7 +10,6 @@ sock.connect(server)
 
 
 def send(message):
-    global sock, server
     print(f'Sending:\n{message}')
     sock.send(message.encode())
     response, _ = sock.recvfrom(1024)
@@ -21,43 +21,24 @@ while True:
     command = input().lower()
     command_parts = command.split(' ')
     if command_parts[0] == 'signup' and len(command_parts) == 3:
-        message_to_send = {
-            'type': 'signup',
-            'username': command_parts[1],
-            'password': command_parts[2],
-        }
+        message_to_send = {'type': 'signup', 'username': command_parts[1], 'password': command_parts[2], }
 
     elif command_parts[0] == 'login' and len(command_parts) == 3:
-        message_to_send = {
-            'type': 'login',
-            'username': command_parts[1],
-            'password': command_parts[2],
-        }
+        message_to_send = {'type': 'login', 'username': command_parts[1], 'password': command_parts[2], }
 
     elif command_parts[0] == 'online' and len(command_parts) == 1:
-        message_to_send = {
-            'type': 'online',
-        }
+        message_to_send = {'type': 'online', }
 
     elif command_parts[0] == 'newgroup' and len(command_parts) == 2:
-        message_to_send = {
-            'type': 'online',
-            'name': command_parts[1],
-        }
+        message_to_send = {'type': 'online', 'name': command_parts[1], }
 
     elif command_parts[0] == 'msgg' and len(command_parts) >= 3:
-        message_to_send = {
-            'type': 'msgg',
-            'group': command_parts[1],
-            'msg': ' '.join(command_parts[2:]),
-        }
+        message_to_send = {'type': 'msgg', 'group': command_parts[1], 'msg': ' '.join(command_parts[2:]), }
 
     elif command_parts[0] == 'msgd' and len(command_parts) >= 3:
-        message_to_send = {
-            'type': 'msgg',
-            'contact': command_parts[1],
-            'msg': ' '.join(command_parts[2:]),
-        }
+        message_to_send = {'type': 'msgg', 'contact': command_parts[1], 'msg': ' '.join(command_parts[2:]), }
+    elif command_parts[0] == 'exit' and len(command_parts) == 1:
+        message_to_send = {'type': 'exit'}
     answer = send(json.dumps(message_to_send))
     print(f'Receiving:\n{answer}')
 
--- Server.py
+++ Server.py
@@ -20,16 +20,35 @@ def answer_signup(username, password, all_users):
         all_users['Users'].append({'username': username, 'password': password})
         with open('Users.txt', 'w') as file:
             file.write(json.dumps(all_users, indent=4))
-        message = {
-            'type': 'OK',
-            'message': 'Signup Successful'
-        }
+        message = {'type': 'OK', 'message': 'Signup Successful'}
         return json.dumps(message)
-    message = {
-        'type': 'ERROR',
-        'message': 'Username is used'
-    }
+    message = {'type': 'ERROR', 'message': 'Username is used'}
     return json.dumps(message)
+
+
+def answer_login(username, password, all_users):
+    for user in all_users['Users']:
+        if user['username'] == username:
+            real_password = user['password']
+            break
+    else:
+        message = {'type': 'ERROR', 'message': 'Username not found'}
+        return json.dumps(message)
+    if real_password == password:
+        message = {'type': 'OK', 'message': 'Login Successful'}
+        online_users.append(username)
+        return json.dumps(message)
+    else:
+        message = {'type': 'ERROR', 'message': 'Password is not correct'}
+        return json.dumps(message)
+
+
+def answer_online():
+    return json.dumps({'type': 'OK', 'online users': list(set(online_users))})
+
+
+def answer_exit():
+    online_users
 
 
 def decode_with_token(encoded_msg):
@@ -40,9 +59,9 @@ def encode_with_token(msg):
     return msg  # TODO
 
 
-users = None
 with open('Users.txt', 'r') as file:
     all_users = json.loads(file.read())
+online_users = []
 
 groups = None
 with open('Groups.txt', 'r') as file:
@@ -58,5 +77,15 @@ while True:
 
         if command_dict['type'] == 'signup':
             response = answer_signup(command_dict['username'], command_dict['password'], all_users)
-
+        elif command_dict['type'] == 'login':
+            response = answer_login(command_dict['username'], command_dict['password'], all_users)
+        elif command_dict['type'] == 'online':
+            print(online_users)
+            response = answer_online()
+        elif command_dict['type'] == 'exit':
+            connection_sock.close()
+            answer_exit()
+            break
+        print(f'Sending:\n{response}')
         connection_sock.send(response.encode())
+    connection_sock.close()
